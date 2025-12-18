5a335718315db73fd52fd9064b91b6c90aa28873
syyy04 <147645852+syyy04@users.noreply.github.com>
2025-12-18 11:55:50 +0100

    Refactor Ambiance system to support multiple instructions
    Updated AmbianceTrigger to handle an array of AmbianceInstructions, allowing multiple actions per trigger. Added support for a new SetParameter action (not yet implemented in AmbianceManager). Refactored AmbianceManager and AmbType into a namespace for better organization, and improved code comments and structure. Updated _TemplateScene.unity to use the new array-based instructions.

--- Unity_3D-Gamekit_Student_2025/Assets/3DGamekitLite/Scripts/CustomScripts/Simon/AmbianceManager.cs
+++ Unity_3D-Gamekit_Student_2025/Assets/3DGamekitLite/Scripts/CustomScripts/Simon/AmbianceManager.cs
@@ -1,87 +1,102 @@
-using System;
-using UnityEngine;
-using FMOD.Studio;
 using FMODUnity;
-public enum AmbType
+using UnityEngine;
+
+namespace _3DGamekitLite.Scripts.CustomScripts.Simon
 {
-    Weather,
-    Outside,
-    Inside,
-}
-
-public class AmbianceManager : MonoBehaviour
-{
-    public static AmbianceManager instance { get; private set; }
-
-    [SerializeField] private StudioEventEmitter weatherEmitter, outsideEmitter, insideEmitter, pointerEmitter;
-
-    private void Awake()
+    public enum AmbType
     {
-        if (instance != null && instance != this)
-        {
-            Destroy(this);
-        }
-        else
-        {
-            instance = this;
-        }
-        DontDestroyOnLoad(this);
+        Weather,
+        Outside,
+        Inside,
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class AmbianceManager : MonoBehaviour
     {
+        // Creating a static variable of this class, which you can get information from, but not write to.
+        public static AmbianceManager instance { get; private set; }
+
+        // StudioEventEmitter variables that hold references to our ambiance events, located in child objects of the AmbianceManager parent object.
+        [SerializeField] private StudioEventEmitter weatherEmitter, outsideEmitter, insideEmitter, pointerEmitter;
+
+        private void Awake()
+        {
+            // Singleton pattern setup, makes sure that there are only one instance of this class
+            if (instance != null && instance != this)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                instance = this;
+            }
+            DontDestroyOnLoad(this);
+        }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
-    }
-
-    public void GetEmitter(AmbType ambType)
-    {
-        switch (ambType)
-        {
-            case AmbType.Weather:
-                pointerEmitter = weatherEmitter;
-                break;
-
-            case AmbType.Inside:
-                pointerEmitter = insideEmitter;
-                break;
-
-            case AmbType.Outside:
-                pointerEmitter = outsideEmitter;
-                break;
-
         }
-    }
 
-    public void PlayEmitter(AmbType ambType)
-    {
-        GetEmitter(ambType);
-        if (!pointerEmitter.IsActive)
+        // GetEmitter is a method that points towards the specific ambianceEmitter that we'd like to instruct different actions to
+        public void GetEmitter(AmbType ambType)
         {
-            pointerEmitter.Play();
-        }
-        else
-        {
-            return;
-        }
-    }
+            // Compares the AmbType that the method receives and the pointerEmitter is initialised with the proper ambianceEmitter.
+            switch (ambType)
+            {
+                case AmbType.Weather:
+                    pointerEmitter = weatherEmitter;
+                    break;
 
-    public void StopEmitter(AmbType ambType)
-    {
-        GetEmitter(ambType);
-        if (pointerEmitter.IsActive)
-        {
-            pointerEmitter.Stop();
+                case AmbType.Inside:
+                    pointerEmitter = insideEmitter;
+                    break;
+
+                case AmbType.Outside:
+                    pointerEmitter = outsideEmitter;
+                    break;
+
+            }
         }
-        else
+
+        public void PlayEmitter(AmbType ambType)
         {
-            return;
+            // Runs the GetEmitter with the information/argument stored in the parameter ambType, so that we can easily play the sound using the pointerEmitter
+            GetEmitter(ambType);
+            
+            // Checks if the pointerEmitter is not already active, if true, then we should play it!
+            if (!pointerEmitter.IsActive)
+            {
+                pointerEmitter.Play();
+            }
+            // If it is active, the return and do nothing...
+            else
+            {
+                return;
+            }
+        }
+
+        public void StopEmitter(AmbType ambType)
+        {
+            // Runs the GetEmitter with the information/argument stored in the parameter ambType, so that we can easily stop the sound using the pointerEmitter
+            GetEmitter(ambType);
+            
+            // Checks if the pointerEmitter is active, if true, then we should stop it!
+            if (pointerEmitter.IsActive)
+            {
+                pointerEmitter.Stop();
+            }
+            // If it is not active, the return and do nothing...
+            else
+            {
+                return;
+            }
         }
     }
 }
--- Unity_3D-Gamekit_Student_2025/Assets/3DGamekitLite/Scripts/CustomScripts/Simon/AmbianceTrigger.cs
+++ Unity_3D-Gamekit_Student_2025/Assets/3DGamekitLite/Scripts/CustomScripts/Simon/AmbianceTrigger.cs
@@ -1,36 +1,45 @@
 using System;
+using _3DGamekitLite.Scripts.CustomScripts.Simon;
 using UnityEngine;
 
 public class AmbianceTrigger : MonoBehaviour
 {
+    // Enum for selecting the action that we want the emitter to do!
     public enum EmitterAction
     {
         Play, 
-        Stop
+        Stop,
+        SetParameter
     }
     
+    // A struct that holds information about what ambianceEmitter we'd like to instruct and the specific parameter information needed for updating a parameter.
     [Serializable]
     public struct AmbianceInstructions
     {
         public AmbType ambType;
         public EmitterAction action;
-        
+        public string parameterName;
+        public float parameterValue;
+
     }
 
-    [SerializeField] private AmbianceInstructions ambInstructions;
+    [SerializeField] private AmbianceInstructions[] ambInstructions;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            switch (ambInstructions.action)
+            foreach (var i in ambInstructions)
             {
-                case EmitterAction.Play:
-                    AmbianceManager.instance.PlayEmitter(ambInstructions.ambType);
-                    break;
-                case EmitterAction.Stop:
-                    AmbianceManager.instance.StopEmitter(ambInstructions.ambType);
-                    break;
+                switch (i.action)
+                {
+                    case EmitterAction.Play:
+                        AmbianceManager.instance.PlayEmitter(i.ambType);
+                        break;
+                    case EmitterAction.Stop:
+                        AmbianceManager.instance.StopEmitter(i.ambType);
+                        break;
+                }
             }
         }
     }
